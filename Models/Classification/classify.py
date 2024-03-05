e8b4cacc24a39f759fe58988625481d3c08d824c
alokillur <alokillur99@gmail.com>
2024-03-06 00:38:53 +0530

    desc added

--- Models/Classification/classify.py
+++ Models/Classification/classify.py
@@ -2,26 +2,27 @@ import numpy as np
 import streamlit as st
 import cv2
 from tensorflow.keras.models import load_model
-import tensorflow as tf
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 
-
-model=load_model('C:\\Users\\aloky\\OneDrive\\Desktop\\Marine_Classification\\Models\\Classification\\marine_model.h5')
+model = load_model('C:\\Users\\aloky\\OneDrive\\Desktop\\Marine_Classification\\Models\\Classification\\marine_model.h5')
 
 class_names = ['Bangus', 'Big Head Carp', 'Black Spotted Barb', 'Catfish', 'Climbing Perch', 'Fourfinger Threadfin', 'Fresherwater Eel', 'Glass Perchlet', 'Goby', 
                'Gold Fish', 'Gourami', 'Grass Crap', 'Green Spotted Puffer', 'Indian Carp', 'Indo Pacific Tarpon', 'Jaguar Fish', 'Janitor Fish', 'Knifefish',
                'Long Snouted Pipefish', 'Mosquito Fish', 'Mudfish', 'Mullet', 'Pangasius', 'Perch', 'Scat Fish', 'Silver Barb', 'Silver Carp', 'Silver Perch', 
                'Snakehead', 'Tenpounder', 'Tilapia']
 
+dangerous_species = ['Green Spotted Puffer', 'Jaguar Fish', 'Janitor Fish', 'Knifefish', 'Long Snouted Pipefish', 'Mosquito Fish', 'Mudfish', 'Snakehead']
 
 st.title('Explore Fishes')
-st.markdown('upload an image of the fish')
+st.markdown('Upload an image of the fish')
 
-fish_image=st.file_uploader("choose an image ",type='jpg')
-submit=st.button('Predict')
+fish_image = st.file_uploader("Choose an image", type='jpg')
 
-if submit:
-    if fish_image is not None:
+if fish_image is not None:
+    st.image(fish_image, caption='Selected Image', use_column_width=True)
+    submit = st.button('Predict')
+
+    if submit:
         file_bytes = np.asarray(bytearray(fish_image.read()), dtype=np.uint8)
         opencv_image = cv2.imdecode(file_bytes, 1)
         opencv_image = cv2.resize(opencv_image, (224, 224))
@@ -33,6 +34,7 @@ if submit:
 
         st.title(result)
 
-        # st.title(str("this is "+result.split('-')[0]+"leaf with "+result.split('-')[1]))
-
-
+        if result in dangerous_species:
+            st.warning(f'The predicted species "{result}" is considered dangerous.')
+        else:
+            st.info(f'The predicted species "{result}" is not considered dangerous.')
