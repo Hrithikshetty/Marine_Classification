dcc9ba16d8c9685ca232258c9d14bf6df6eaab2a
alokillur <alokillur99@gmail.com>
2024-06-16 08:58:07 +0530

    map added

--- Models/Constants.py
+++ Models/Constants.py
@@ -4,4 +4,4 @@ data_path = 'C:\\Users\\aloky\\OneDrive\\Desktop\\Marine_Classification\\Models\
 
 class_names = ['Bangus', 'Big Head Carp', 'Black Spotted Barb', 'Catfish', 'Climbing Perch', 'Fourfinger Threadfin', 'Fresherwater Eel', 'Glass Perchlet', 'Goby', 'Gold Fish', 'Gourami', 'Grass Crap', 'Green Spotted Puffer', 'Indian Carp', 'Indo Pacific Tarpon', 'Jaguar Fish', 'Janitor Fish', 'Knifefish','Long Snouted Pipefish', 'Mosquito Fish', 'Mudfish', 'Mullet', 'Pangasius', 'Perch', 'Scat Fish', 'Silver Barb', 'Silver Carp', 'Silver Perch', 'Snakehead', 'Tenpounder', 'Tilapia']
 
-api_key = "sk-proj-N47bCsJ7S46aFMcYFPg3T3BlbkFJYa8IxSU3SnDa1v4E3k9z"
+api_key = ""
